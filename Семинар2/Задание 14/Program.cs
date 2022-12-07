f04de6b357ca0651090b99a98b19de4690e6cf71
Dina Filippova <dinafilippova@gmail.com>
2022-12-07 14:54:38 +0700

    число кратное 7 и 23

---
+++ Семинар2/Задание 14/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Пожалуйста, введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int remainder1 = number1 % 7;
+int remainder2 = number1 % 23;
+if (remainder1 == 0 && remainder2 == 0) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
+
+
