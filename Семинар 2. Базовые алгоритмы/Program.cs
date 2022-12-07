c7832991928ae2de7951469aa2e9561f73834620
Dina Filippova <dinafilippova@gmail.com>
2022-12-07 12:54:52 +0700

    Random

---
+++ Семинар 2. Базовые алгоритмы/Program.cs
@@ -0,0 +1,12 @@
+﻿Random random = new Random();
+int number = 0;
+number = random.Next(10, 100);
+Console.WriteLine(number);
+int b = number % 10;
+int a = number / 10;
+if (a > b)
+Console.WriteLine("a > b");
+else if (b > a)
+Console.WriteLine("b > a");
+else if (a == b)
+Console.WriteLine("b == a");
