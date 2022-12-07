d6b55a618001928e57217e3de30cbf0b3a953980
Dina Filippova <dinafilippova@gmail.com>
2022-12-07 13:22:42 +0700

    конкетированные строки

--- Семинар 2. Базовые алгоритмы/Program.cs
+++ Семинар 2. Базовые алгоритмы/Program.cs
@@ -1,12 +1,16 @@
-﻿Random random = new Random();
-int number = 0;
-number = random.Next(10, 100);
-Console.WriteLine(number);
-int b = number % 10;
-int a = number / 10;
-if (a > b)
-Console.WriteLine("a > b");
-else if (b > a)
-Console.WriteLine("b > a");
-else if (a == b)
-Console.WriteLine("b == a");
+﻿// Random random = new Random();
+// int number = 0;
+// number = random.Next(10, 100);
+// Console.WriteLine(number);
+// int b = number % 10;
+// int a = number / 10;
+// if (a > b)
+// Console.WriteLine("a > b");
+// else if (b > a)
+// Console.WriteLine("b > a");
+// else if (a == b)
+// Console.WriteLine("b == a");
+
+string text = "la";
+string example = "lalalala";
+Console.WriteLine($"Тырыпыры {text+example} растопыры");
