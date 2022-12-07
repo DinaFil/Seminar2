f4ee1b53cc27d4e150da79709723e834769f553e
Dina Filippova <dinafilippova@gmail.com>
2022-12-07 14:41:51 +0700

    кратность с остатком

---
+++ Семинар2/Задание 3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Пожалуйста, введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int remainder = number1 % number2;
+if (remainder == 0) Console.WriteLine("Первое число кратно второму числу");
+else Console.WriteLine($"Первое число не кратно второму, остаток {remainder}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Семинар2/Задание2/Program.cs
@@ -0,0 +1,8 @@
+﻿Random random = new Random();
+int number = random.Next(100,1000);
+Console.WriteLine(number);
+int a = number / 100;
+int b = number % 10;
+
+Console.WriteLine($"Двухзначное числое {a}{b}");
+
